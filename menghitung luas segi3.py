a25e61a61e428c906d0bea0ee203b72c06c703cf
yudhistira2805 <71997778+yudhistira2805@users.noreply.github.com>
2020-10-05 11:15:48 +0700

    Add files via upload
    Segi3 gank

---
+++ menghitung luas segi3.py
@@ -0,0 +1,8 @@
+print("Menghitung Luas Segi3")
+
+a = float(input("\nMasukan Alas   : "))
+t = float(input("\nMasukan Tinggi : "))
+          
+luas = 0.5*a*t
+
+print("Luas Segi3\t : ",luas)
---
+++ menghitung segi3 sama sisi.py
@@ -0,0 +1,10 @@
+print("Menghitung Keliling & Luas Segi3 Sama Sisi")
+
+s = float(input("\nMasukan Sisi   : "))
+t = float(input("\nMasukan Tinggi : "))
+          
+luas = 0.5*s*t
+keliling = 3*s
+
+print("Luas Segi3\t : ",luas)
+print("Keliling Segi3\t : ",keliling)
